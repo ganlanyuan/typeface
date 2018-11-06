741b88cf7d8971963fcf968e19d3346d8757e7ea
William Lin 22 <ganlanyuan@users.noreply.github.com>
2018-11-06 07:38:27 -0500

    add range

--- src/js/script.js
+++ src/js/script.js
@@ -3,7 +3,23 @@ let doc = document,
     html = doc.documentElement,
     body = doc.body;
 
+html.className = html.className.replace('no-script', 'script');
+
 win.addEventListener('click', (e) => {
   let target = e.target;
   if (target.classList.contains('i-expand')) { target.parentNode.classList.toggle('open'); }
-})
+});
+
+if (win.NodeList && !NodeList.prototype.forEach) {
+    NodeList.prototype.forEach = Array.prototype.forEach;
+}
+
+let ranges = doc.querySelectorAll('[type="range"]');
+ranges.forEach(function(range) {
+  range.parentNode.style.fontSize = range.getAttribute('data-base') + 'px';
+  range.addEventListener('change', function(e) {
+    let el = e.target,
+        parent = el.parentNode;
+        parent.style.fontSize = (Number(el.getAttribute('data-base')) + Number(el.getAttribute('data-step')) * el.value) + 'px';
+  });
+});
